4ba28c9bec274ae3832ecf0941d1021209b3da59
Ping <144251747+w0436300@users.noreply.github.com>
2024-03-24 15:29:56 -0300

    update header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,40 +4,41 @@ const NavBar = () => {
         <div className="navbar bg-blue-800">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-white">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white">
-                        <li><a>Item 1</a></li>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href='https://www.koii.network/node'>NODES</a></li>
                         <li>
-                            <a>Parent</a>
+                            <a href='https://docs.koii.network/'>DOCS</a>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                            <li><a>Become a Webs Developer</a></li>
+                                <li><a>Earn Passive Income </a></li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><a href='https://www.koii.network/'>JOIN</a></li>
                     </ul>
                 </div>
                 <a href='/' className="btn btn-ghost text-xl text-white">KOII</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-white">
-                    <li><a>Item 1</a></li>
+                    <li><a href='https://www.koii.network/node'>NODES</a></li>
                     <li>
                         <details>
-                            <summary>Parent</summary>
+                            <summary>DOCS</summary>
                             <ul className="p-2 text-black">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li><a href='https://docs.koii.network/'>summary</a></li>
+                                <li><a href='https://docs.koii.network/quickstart/hello-world/introduction'>Become a Developer</a></li>
+                                <li><a href='https://koii.network/node'>Earn Passive Income </a></li>
                             </ul>
                         </details>
                     </li>
-                    <li><a>Item 3</a></li>
+                    <li><a href='https://blog.koii.network/'>BLOG</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a href='https://www.koii.network/' className="btn">Join the Network</a>
             </div>
         </div>
 
